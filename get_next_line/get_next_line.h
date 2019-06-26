e10051d5d5e94649650d1f2606cae1af32842cc8
Verona SINGH <vesingh@c4r20s1.wethinkcode.co.za>
2019-06-26 06:45:09 +0200

    updates?

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 09:11:24 by vesingh           #+#    #+#             */
-/*   Updated: 2019/06/24 16:33:32 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/06/24 16:51:47 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 13
+# define BUFF_SIZE 30
 # include "./libft/libft.h"
 
 int					get_next_line(const int fd, char **line);
