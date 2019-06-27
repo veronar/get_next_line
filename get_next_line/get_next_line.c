29f8308edadb080ff711aa5b797937bf43669a03
Verona SINGH <vesingh@c4r20s1.wethinkcode.co.za>
2019-06-27 15:28:38 +0200

    gnl works, but frans broke it

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 08:28:55 by vesingh           #+#    #+#             */
-/*   Updated: 2019/06/27 13:17:12 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/06/27 15:28:04 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 static int	ft_storetoline(char **store, char **line)
 {
@@ -24,22 +23,13 @@ static int	ft_storetoline(char **store, char **line)
 		*line = ft_strsub(*store, 0, len);
 		temp = ft_strchr(*store, '\n') + 1;
 		temp = ft_strdup(temp);
-		//ft_putendl(temp);
-		//temp = ft_strsub(*store, len + 1, ft_strlen(*store) - len);
-		//printf("temp = %s\n", temp);
-		//*store = ft_strchr(*store, '\n') + 1;
 		ft_strdel(store);
 		*store = ft_strdupdel(&temp);
-		//sleep(1);
-		//printf("line = %s\t store = %s\n", *line, *store);
-		//sleep(1);
 		if (*store[0] == '\0')
 			ft_strdel(store);
-			//*store = ft_strnew(0);
 	}
 	else if (ft_strchr(*store, '\0'))
 	{
-	//	len = ft_strchr(*store, '\0') - *store;
 		*line = ft_strdupdel(store);
 	}
 	return (1);
@@ -47,37 +37,26 @@ static int	ft_storetoline(char **store, char **line)
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	*store = NULL;
+	static char	*store[255];
 	char		buff[BUFF_SIZE + 1];
-//	char		*temp;
 	int			n;
 
-	if (!store)
-		store = ft_strnew(0);
-	if (fd < 0 || line == NULL)
+	if (fd < 0 || line == NULL || BUFF_SIZE < 0)
 		return (-1);
+	if (!store[fd])
+		store[fd] = ft_strnew(0);
 	while ((n = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[n] = '\0';
-		store = ft_strjoinfree(store, buff);
-//		printf("buff = %s\n", buff);
-		//if (store == NULL)
-		//	store = ft_strnew(0);
-	//	temp = ft_strjoin(store, buff);
-		//printf("temp= %s\n", temp);
-		//free(store);
-		//if (store != NULL)
-		//	ft_strdel(&store);
-	//	store = ft_strdupdel(&temp);
-		//printf("store = %s\n", store);
-		if (ft_strchr(store, '\n'))
+		store[fd] = ft_strjoinfree(store[fd], buff);
+		if (ft_strchr(store[fd], '\n'))
 			break ; 
 	}
 	if (n < 0)
 		return (-1);
-	else if (n == 0 && (store == NULL || store[0] == '\0'))
+	else if (n == 0 && (store[fd] == NULL || store[fd][0] == '\0'))
 		return (0);
 	else 
-		return(ft_storetoline(&store, line));
+		return(ft_storetoline(&store[fd], line));
 	return (0);
 }
